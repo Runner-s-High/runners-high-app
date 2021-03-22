355aa352155a307887837b721ec02d8cdde4da89
wrh17b <74798057+wrh17b@users.noreply.github.com>
2021-03-22 11:05:21 -0400

    Added quotes to home fragment

--- app/src/main/java/com/codepath/runnershigh/fragments/HomeFragment.java
+++ app/src/main/java/com/codepath/runnershigh/fragments/HomeFragment.java
@@ -1,5 +1,6 @@
 package com.codepath.runnershigh.fragments;
 
+import android.content.res.Resources;
 import android.os.Bundle;
 
 import androidx.annotation.NonNull;
@@ -15,13 +16,17 @@ import android.widget.TextView;
 import com.codepath.runnershigh.R;
 
 import java.text.SimpleDateFormat;
+import java.util.Arrays;
 import java.util.Calendar;
+import java.util.List;
+import java.util.Random;
 
 
 public class HomeFragment extends Fragment {
     TextClock tcDate;
     TextClock tcTime;
 
+    TextView tvQuote;
 
     public HomeFragment() {
         // Required empty public constructor
@@ -49,11 +54,21 @@ public class HomeFragment extends Fragment {
         super.onViewCreated(view, savedInstanceState);
         tcDate = view.findViewById(R.id.tcDate);
         tcTime = view.findViewById(R.id.tcTime);
+        tvQuote = view.findViewById(R.id.tvQuote);
 
+        //Set format for date and time
         tcDate.setFormat12Hour("EEE, MMM d, ''yy");
         tcTime.setFormat12Hour("h:mm a");
 
+        //Get quotes from resources and set text view to random quote
+        Resources resources = getResources();
+        List<String> Quotes = Arrays.asList(resources.getStringArray(R.array.Quotes));
+        Random random =new Random();
+        tvQuote.setText(Quotes.get(
+                random.nextInt(
+                        Quotes.size())));
 
+        //TODO: set up most recent run
 
     }
 
